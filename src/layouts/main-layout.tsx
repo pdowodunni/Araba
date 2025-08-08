3d928b1ad23a085ad84c27fc56b473005f122cf6
Tolux001 <toluwalasepeter100@gmail.com>
2025-08-08 09:46:50 +0100

    service page

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,18 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Footer from "../components/shared/footer";
 import OurWork from "../pages/our-work";
+import Service from "../pages/services";
 
 function MainLayout() {
-  return (
-    <>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/our-work" element={<OurWork />} />
-      </Routes>
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavigationBar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/our-work" element={<OurWork />} />
+				<Route path="/service" element={<Service />} />
+			</Routes>
+			<Footer />
+		</>
+	);
 }
 
 export default MainLayout;
