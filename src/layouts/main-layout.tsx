41be70bb5ca59a09d2d8854ce40fb18c89f4a83d
David <topghostly@gmail.com>
2025-08-13 18:11:51 +0100

    ready for merge

--- src/layouts/main-layout.tsx
+++ src/layouts/main-layout.tsx
@@ -3,20 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Footer from "../components/shared/footer";
 import OurWork from "../pages/our-work";
-import Service from "../pages/servicesVoice";
 
 function MainLayout() {
-	return (
-		<>
-			<NavigationBar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/our-work" element={<OurWork />} />
-				<Route path="/voiceover-service" element={<Service />} />
-			</Routes>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/our-work" element={<OurWork />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
