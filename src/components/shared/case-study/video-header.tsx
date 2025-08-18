af36b7d5b651e71a6aeafcec0cce6b6375156ddf
Tolux001 <toluwalasepeter100@gmail.com>
2025-08-18 18:24:03 +0100

    Feat: Youtube videos

--- src/components/shared/case-study/video-header.tsx
+++ src/components/shared/case-study/video-header.tsx
@@ -1,43 +1,50 @@
 import { useState } from "react";
+import ReactPlayer from "react-player";
+
+// ==========================
+// I am using react-player for it
+// ==========================
 
 function CaseStudyVideoHeader({
-  videoUrl,
-  videoFormat,
-  poster,
+	videoUrl,
+	poster,
 }: {
-  videoUrl: string;
-  videoFormat?: string;
-  poster?: string;
+	videoUrl: string;
+	videoFormat?: string;
+	poster?: string;
 }) {
-  const [loaded, setLoaded] = useState(false);
-  return (
-    <section>
-      <div className="max-w-[1680px] mx-auto px-0 lg:px-8">
-        <div className="py-sm-pad">
-          <div className="w-full relative aspect-video bg-mid-bg lg:rounded-lg overflow-hidden">
-            <video
-              playsInline
-              autoPlay
-              muted
-              loop
-              onLoadedData={() => setLoaded(true)}
-              poster={poster}
-              className="absolute inset-0 w-full h-full object-cover"
-            >
-              <source src={videoUrl} type={videoFormat ?? "video/mp4"} />
-            </video>
-            {!loaded && (
-              <img
-                src="/images/thumb.jpg"
-                alt="background logo"
-                className="opacity-0"
-              />
-            )}
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	const [loaded, setLoaded] = useState(false);
+	return (
+		<section>
+			<div className="max-w-[1680px] mx-auto px-0 lg:px-8">
+				<div className="py-sm-pad">
+					<div className="w-full relative aspect-video bg-mid-bg lg:rounded-lg overflow-hidden">
+						<ReactPlayer
+							src={videoUrl}
+							width="100%"
+							height="100%"
+							playing={true}
+							muted={true}
+							loop={true}
+							playsInline={true}
+							config={{
+								youtube: { rel: 0 },
+							}}
+							onReady={() => setLoaded(true)}
+							className="absolute top-0 left-0 w-full h-full"
+						/>
+						{!loaded && poster && (
+							<img
+								src={poster}
+								alt="Video poster"
+								className="absolute top-0 left-0 w-full h-full object-cover"
+							/>
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
 export default CaseStudyVideoHeader;
