94142790b0efafc581db2fa630f62468e05e2727
David <topghostly@gmail.com>
2025-10-17 08:09:37 +0100

    feat: update Resources link in navigator to point to the correct URL

--- src/config/navigator.ts
+++ src/config/navigator.ts
@@ -3,6 +3,6 @@ import ServicesDropDown from "../components/nav-bar/drop-down/services";
 export const NAV_LINKS = [
   { label: "Services", href: "#", dropDown: ServicesDropDown },
   { label: "Our work", href: "/our-work" },
-  { label: "Resources", href: "#" },
+  { label: "Resources", href: "https://arabacollective.co/r" },
   { label: "About Us", href: "#" },
 ];
